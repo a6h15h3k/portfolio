ed853ce77dc293d4798c95cdf1db9d0214dcc00e
a6hi <saraffabhishek@outlook.com>
2022-11-04 18:28:10 -0400

    Added Side-bar

--- react-portfolio/src/components/Layout/index.js
+++ react-portfolio/src/components/Layout/index.js
@@ -1,7 +1,26 @@
-import './index.scss';
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../Sidebar'
+import './index.scss'
 
 const Layout = () => {
-    return <>Hello</>
+  return (
+    <div>
+      <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;html&gt;
+        <br/>
+        <span className="top-tag-body">&lt;body&gt;</span>
+        </span>
+
+        <Outlet />
+
+        <span className="tags bottom-tags">&lt;/body&gt;
+        <br/>
+        <span className='bottom-tag-html'>&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
+  )
 }
 
-export default Layout;
+export default Layout
